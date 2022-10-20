c32c3772af58add347e050588496056c8a557b18
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2022-10-19 19:59:59 -0500

    modify boot

--- default/boot.py
+++ default/boot.py
@@ -1,34 +1,59 @@
 from machine import I2C, Pin, reset
 from ssd1306 import SSD1306_I2C
 from env import wifi_name, wifi_pass
+from jco import Numbersbuffer
 import time
+import uos
+import network
 try:
     import webrepl
 except:
     print("not fount webrepl")
     time.sleep(3)
     reset()
-import uos
-import network
 
 try:
     oled = SSD1306_I2C(128, 32, I2C(0,scl=Pin(22), sda=Pin(21)))
     oled.fill(0)
     oled.invert(False)
     oled.contrast(1)
-    oled.text("start",int((128/2)-40),int((32/2)-10))
+    oled.text("init",int((128/2)-40),int((32/2)-10))
     oled.show()
     time.sleep(2)
     oled.fill(0)
 except:
     print("not fount display")
 
-station = network.WLAN(network.STA_IF)
-if station.active() == False:
-    station.active(True)
-    station.connect(wifi_name, wifi_pass)
-    while station.isconnected() == False:
+wifi = network.WLAN(network.STA_IF)
+if wifi.active() == False:
+    wifi.active(True)
+    wifi.connect(wifi_name, wifi_pass)
+    while wifi.isconnected() == False:
         pass
 else:
         print("is connected")
+        
 webrepl.start()
+
+numbers = Numbersbuffer([
+    "0.pbm",
+    "1.pbm",
+    "2.pbm",
+    "3.pbm",
+    "4.pbm",
+    "5.pbm",
+    "6.pbm",
+    "7.pbm",
+    "8.pbm",
+    "9.pbm"
+])
+
+if oled:
+    oled.fill(0)
+    oled.contrast(1)
+    oled.graphic(numbers.Render(str(wifi.ifconfig()[0].split(".")[3])[0],24,32),4*24,0)
+    oled.graphic(numbers.Render(str(wifi.ifconfig()[0].split(".")[3])[1],24,32),3*24,0)
+    oled.graphic(numbers.Render(str(wifi.ifconfig()[0].split(".")[3])[2],24,32),2*24,0)
+    oled.show()
+else:
+    print(wifi.ifconfig()[0].split(".")[3])
